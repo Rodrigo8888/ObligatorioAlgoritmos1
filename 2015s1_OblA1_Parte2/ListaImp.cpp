a2393671838c1f40f5c1ff81201371b271ac0909
Fabiana Pedrini <fpedrini19@gmail.com>
2015-05-30 12:14:17 -0300

    ListasImp

--- 2015s1_OblA1_Parte2/ListaImp.cpp
+++ 2015s1_OblA1_Parte2/ListaImp.cpp
@@ -13,103 +13,176 @@ ostream &operator<< <>(ostream& out, const ListaImp<T> &l) {
 
 template <class T>
 ListaImp<T>::ListaImp(){
-	//NO IMPLEMENTADO
+	this->ppio = this ->fin = NULL;
+	this->largo =0;
 }
 
 template <class T>
 ListaImp<T>::ListaImp(const Lista<T> &l) {
-	//NO IMPLEMENTADO
+	largo=0;
+	*this=l;
 }
 
 template <class T>
 ListaImp<T>::ListaImp(const ListaImp<T> &l) {
-	//NO IMPLEMENTADO
+	largo=0;
+	*this=l;
 }
 
 template <class T>
 Lista<T> &ListaImp<T>::operator=(const Lista<T> &l) { 
-	if (this != &l) {
+		virtual Lista<T> &operator=(const Lista<T> &l) {
+		if (this != &l) {
+			this->Vaciar();
 
-		//NO IMPLEMENTADO
-
+			for (Iterador<T> &i = l.GetIterador(); !i.EsFin();) {
+				AgregarFin(i++);
+			}
+		}
+		return *this;
 	}
-	return *this;
 }
 
 template <class T>
 Lista<T> &ListaImp<T>::operator=(const ListaImp<T> &l) { 
-	if (this != &l) {
+		virtual Lista<T> &operator=(const Lista<T> &l) {
+		if (this != &l) {
+			this->Vaciar();
 
-		//NO IMPLEMENTADO
-
+			for (Iterador<T> &i = l.GetIterador(); !i.EsFin();) {
+				AgregarFin(i++);
+			}
+		}
+		return *this;
 	}
-	return *this;
 }
 
 template <class T>
 ListaImp<T>::~ListaImp(){
-	//NO IMPLEMENTADO
+	this->Vaciar();
 }
 
 template <class T>
 void ListaImp<T>::AgregarPpio(const T &e){
-	//NO IMPLEMENTADO
+	NodoLista<T> *nuevo = new NodoLista<T> (e,NULL,this->ppio);
+	if (this->EsVacia()) this->ppio = this->fin = nuevo;
+	else{
+		this->ppio->ant = nuevo;
+		this->ppio = nuevo;
+	}
+	this->largo ++;
 }
 
 template <class T>
 void ListaImp<T>::AgregarFin(const T &e){
-	//NO IMPLEMENTADO
+	NodoLista<T> *nuevo = new NodoLista<T>(e,this->fin, NULL);
+	if (this->EsVacia()) this->ppio = this->fin = nuevo;
+	else{
+		this->fin->sig = nuevo;
+		this->fin = nuevo;
+	}
+	this->largo ++;
 }
 
 template <class T>
 void ListaImp<T>::AgregarOrd(const T &e){
-	//NO IMPLEMENTADO
+	if (!this->EsVacia()){
+		if (e<= this->ppio->dato) this->AgregarPpio(e);
+		else if (e>= this->fin->dato) this->AgregarFin(e);
+		else{
+			NodoLista<T> *aux = this->ppio;
+			while (aux->dato <e){
+				aux = aux->sig;
+			}
+			NodoLista<T> *nuevo = new NodoLista <T> (e, aux->ant, aux);
+			aux->ant->sig = nuevo;
+			aux->ant = nuevo;
+			this->largo ++;
+		}		
+	}
+	else this->AgregarPpio();
 }
 
 template <class T>
 void ListaImp<T>::BorrarPpio(){
-	//NO IMPLEMENTADO
+	if(!this->EsVacia()){
+		NodoLista<T>* aux = this->ppio;
+		this->ppio = this->ppio->sig;
+		delete(aux);
+		if(this->largo==1) this->ppio = this->fin = NULL;
+		else this->ppio->ant = NULL;
+		this->largo--;
+	}
 }
 
 template <class T>
 void ListaImp<T>::BorrarFin(){
-	//NO IMPLEMENTADO
+	if(!this->EsVacia){
+		NodoLista<T>* aux = this->fin;
+		this->fin = this->fin->ant;
+		delete(aux);
+		if(this->largo==1) this->ppio=this->fin=NULL;
+		else this->fin->sig = NULL;
+		largo--;
+
+	}
 }
 
 template <class T>
 void ListaImp<T>::Borrar(const T &e){
-	//NO IMPLEMENTADO
+	if (this->ppio->dato == e) this->BorrarPpio();
+	else if (this->fin->dato == e) this->BorrarFin();
+	else{
+		NodoLista<T> *aux = this->ppio->sig;
+		while (aux){
+			if (aux->dato == e){
+				aux->ant->sig = aux->sig;
+				aux->sig->ant = aux->ant;
+				delete aux;
+				this->largo--;
+			}
+			aux = aux->sig;
+		}
+	}
 }
 
 template <class T>
 const T& ListaImp<T>::Recuperar(const T &e) const {
-
-	//NO IMPLEMENTADO
-	return *(new T()); 
+	NodoLista <T> *aux= this->ppio;
+	bool ok = false;
+	while (aux&& !ok){
+		if (aux->dato == e) ok = true;
+		else aux = aux->sig;
+	}
+	return aux->dato;
 }
 
 template <class T>
 bool ListaImp<T>::Existe(const T &e) const {
-
-	//NO IMPLEMENTADO	
-	return false;
+	NodoLista <T> *aux= this->ppio;
+	bool ok = false;
+	while (aux&& !ok){
+		if (aux->dato == e) ok = true;
+		else aux = aux->sig;
+	}
+	return ok;
 }
 
 template <class T>
 void ListaImp<T>::Vaciar(){
-	//NO IMPLEMENTADO	
+	while (!this->EsVacia()){
+		this->BorrarFin();
+	}
 }
 
 template <class T>
 unsigned int ListaImp<T>::Largo()const { 
-	//NO IMPLEMENTADO	
-	return 0; 
+	return this->largo; 
 }
 
 template <class T>
-bool ListaImp<T>::EsVacia()const { 
-	//NO IMPLEMENTADO	
-	return false; 
+bool ListaImp<T>::EsVacia()const { 	
+	return this->largo==0; 
 }
 
 template <class T>
